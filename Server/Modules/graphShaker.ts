1c211bc7f0d9cc098c2d193209fc13406d737a3c
Ghoost <ghoost724@gmail.com>
2026-02-16 13:40:15 +0200

    More ChartShaker shenanigans

--- Server/Modules/graphShaker.ts
+++ Server/Modules/graphShaker.ts
@@ -111,9 +111,10 @@ interface lineGrapthModel {
     height:number
     x:string
     y:string
-    data:object[]
+    data:any[]
     verticals:number
-    horizontals:number 
+    horizontals:number
+    fontSize:number
 }
 
 function lineGrap(config:lineGrapthModel) {
@@ -125,23 +126,32 @@ function lineGrap(config:lineGrapthModel) {
         vLines.push(((t - start) / (end - start)) * width);
     } */
 
-    // Horizontal grid lines
+    // Horizontal X grid lines
     for (let i:number = 0; i < config.height; i + config.height / (config.verticals + 1)) {
-
+        // 
     }
 
     // Compute Y scaling
-    const values = sensor.data.map(d => d.value);
-    const min = Math.min(...values);
-    const max = Math.max(...values);
-    const range = max - min || 1;
+    const valuesY = config.data.map(d => d.value);
+    const minY = Math.min(...valuesY);
+    const maxY = Math.max(...valuesY);
+    const rangeY = Math.floor(maxY - minY) + 2 || 1;
+
+    // Compute X scaling
+    const valuesX = config.data.map(d => d.value);
+    const minX = Math.min(...valuesX);
+    const maxX = Math.max(...valuesX);
+    const rangeX = Math.floor(maxX - minX) + 2 || 1;
+
+    const nameX = "time";
+    const namey = "value";
 
     // Build data points and path
     let pathD = "";
-    const points = sensor.data.map((d, i) => {
-        const timestamp = new Date(d.time).getTime();
-        const x = ((timestamp - start) / (end - start)) * width;
-        const y = height - ((d.value - min) / range) * height;
+    const points = config.data.map((d, i) => {
+        const timestamp = new Date(d[nameX]).getTime();
+        const x = ((timestamp - minX) / (maxX - minX)) * config.width;
+        const y = config.height - ((d[namey] - minY) / rangeY) * config.height;
 
         if (i === 0) pathD += `M${x} ${y}`;
         else pathD += ` L${x} ${y}`;
@@ -149,4 +159,28 @@ function lineGrap(config:lineGrapthModel) {
         return { x, y };
     });
 
+    return `
+        <svg width="100%" height="100%" 
+            viewbox="0 0 auto auto" 
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg">
+
+            <rect  width="${config.width}" height="${config.height}/">
+
+        </svg>
+    `;
+
 };
+
+
+/*
+returns {
+    grapth_width --> for graph vewbox
+    graph_height --> for graph vewbox
+
+    graph_line --> <path />
+    graph_points --> [<circle />, ...]
+
+    graph_verticals <g></g>
+}
+*/
