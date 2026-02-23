8ddb8c0f8dcfdb8a6d095b00ea3b6934d6d78298
Ghoost <ghoost724@gmail.com>
2026-02-23 22:50:58 +0200

    api route sub-router file names changed, add a login sub-route

---
+++ Server/Routes/api/_login.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+
+import UserSchema from '../../Lib/mongoDB_models/User_Schema';
+
+const router = Router();
+
+// ------------------------- Login -------------------------
+
+// API Login POST endpoint
+router.post('/', async (req, res) => {
+    let body = req.body || {};
+
+    // Parse 'user' and 'pass' and check if defined
+    let username = body.username
+    let password = body.password
+
+    if(!username || !password) {
+        return res.status(400).json({
+            error: "credentials not provided"
+        });
+    }
+
+    // Get user data
+    let user = await UserSchema.findOne({ username: username });
+    
+    // Validate Username
+    if(!user || !bcrypt.compare(password, user.password)) {
+        return res.status(401).json({
+            error: "invalid credentials"
+        });
+    }
+
+    // Sign an JWT authentication token
+    if (!process.env.JWT_SECRET) {
+        return res.status(401).json({
+            error: "server error"
+        });
+    }
+    const token = jwt.sign(
+        { 
+            id: user._id,
+            iat: Date.now()
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+
+    // Put token with responses cookies
+    return res.json({
+        auth_token: token 
+    });
+});
+
+export default router;
--- Server/Routes/api/index.ts
+++ Server/Routes/api/index.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express';
-import sensors_route from './sensors';
-import server_info_route from './serverData';
-import profile_route from './profile';
+import sensors_route from './_sensors';
+import server_info_route from './_serverData';
+import profile_route from './_profile';
+import login_route from './_login';
 
 const router = Router();
 router.use('/profile', profile_route);
 router.use('/sensors', sensors_route);
 router.use('/server', server_info_route);
+router.use('/login', server_ilogin_routenfo_route);
 
 export default router;
